cdfbb07d90cc329d27a35861df2748eb34d932ff
iHubG <ianmacalinao50@gmail.com>
2025-09-02 15:01:43 +0800

    adjusted brands height

--- src/components/Brands.tsx
+++ src/components/Brands.tsx
@@ -9,14 +9,14 @@ const Brands = () => {
     <div className="bg-black py-10 px-5 lg:px-15">
       <div className="flex flex-col items-center gap-6 lg:flex-row lg:justify-between lg:items-center lg:gap-10">
         <div className="flex gap-10 lg:gap-6 lg:contents">
-          <img src={Versace} alt="versace" className="h-6 lg:h-8 w-auto filter brightness-0 invert" />
-          <img src={Zara} alt="zara" className="h-6 lg:h-8 w-auto filter brightness-0 invert" />
-          <img src={Gucci} alt="gucci" className="h-6 lg:h-8 w-auto filter brightness-0 invert" />
+          <img src={Versace} alt="versace" className="h-5 lg:h-8 w-auto filter brightness-0 invert" />
+          <img src={Zara} alt="zara" className="h-5 lg:h-8 w-auto filter brightness-0 invert" />
+          <img src={Gucci} alt="gucci" className="h-5 lg:h-8 w-auto filter brightness-0 invert" />
         </div>
         
         <div className="flex gap-6 lg:contents">
-          <img src={Prada} alt="prada" className="h-6 lg:h-8 w-auto filter brightness-0 invert" />
-          <img src={CalvinKlein} alt="calvin klein" className="h-6 lg:h-8 w-auto filter brightness-0 invert" />
+          <img src={Prada} alt="prada" className="h-5 lg:h-8 w-auto filter brightness-0 invert" />
+          <img src={CalvinKlein} alt="calvin klein" className="h-5 lg:h-8 w-auto filter brightness-0 invert" />
         </div>
       </div>
     </div>
